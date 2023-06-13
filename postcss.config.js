ac306ef1f2868046afa79c98ca745228357bdfe6
paulrvach <paulrvach@gmail.com>
2023-06-13 18:28:09 -0400

    added tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   // Add you postcss configuration here
   // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
-  plugins: [["autoprefixer"]],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css'; //added line
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, Redux Toolkit!</h1>
+      <h1 className='text-3xl'>Hello, Redux Toolkit!</h1>
       {/* Add your components and logic here */}
     </div>
   );
