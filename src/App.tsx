f3345b94bcbe535347960e075307518f31ed1f4b
Curtis.Lovrak <curtislovrak@gmail.com>
2023-06-13 17:08:27 -0400

    installed and built skeleton for tsx redux toolkit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, Redux Toolkit!</h1>
+      {/* Add your components and logic here */}
+    </div>
+  );
+};
+
+export default App;
