3a770731ce99b9f20b11ea5d927d3f4ea30170f3
Owner <Owner@MacBook-Pro.local>
2023-06-13 18:27:20 -0700

    finished the drawing board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'; //added line
+import Board from './Board'
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1 className='text-3xl'>Hello, Redux Toolkit!</h1>
+      <h1 className='text-3xl'>Welcome to DrawRTC</h1>
+      <Board></Board>
       {/* Add your components and logic here */}
     </div>
   );
