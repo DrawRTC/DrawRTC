afbef1dba84176f6b73df20996470afe9a718fe4
Ariel Lin <al2248@cornell.edu>
2023-06-15 03:16:38 -0400

    restyled board

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import Board from './components/Board'
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Welcome to DrawRTC</h1>
+    <>
       <Board />
-      {/* Add your components and logic here */}
-    </div>
+    </>
   );
 };
 
