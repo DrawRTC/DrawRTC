70983173e08230af0d07e4e5359f3d8925392474
Ariel Lin <al2248@cornell.edu>
2023-06-15 10:11:10 -0400

    update socket link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false,
+  darkMode: 'media',
   theme: {
     extend: {},
   },
